e375a9e2b114931022f9aef3665abe01da364aae
Raghavan <oneraghavan@gmail.com>
2015-09-26 09:31:37 +0530

    #fpinscalaexercise 2.2

---
+++ bookExercise/fpinscala/chapter2/IsSorted-2_2.scala
@@ -0,0 +1,19 @@
+
+object isSorted {
+
+  def main(args: Array[String]) {
+    val ordered: (Int, Int) => Boolean = (A: Int, B: Int) => A < B
+
+
+    def isSorted[A](as: Array[A], ordered: (A, A) => Boolean): Boolean = {
+      val length: Int = as.length
+      length match {
+        case 1 => true
+        case n => ordered(as(n - 2), as(n - 1)) && isSorted(as.slice(0, n - 1), ordered)
+      }
+
+    }
+    println(isSorted(Array(1, 2, 3, 4, 5), ordered))
+  }
+
+}
