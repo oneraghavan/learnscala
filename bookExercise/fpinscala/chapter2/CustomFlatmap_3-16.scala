5588bde08e83337c3b6f50abfa2cd823ad6ac7bc
Raghavan <oneraghavan@gmail.com>
2015-09-29 22:02:44 +0530

    #fpinscala more exercises in chapter 3

---
+++ bookExercise/fpinscala/chapter2/CustomFlatmap_3-16.scala
@@ -0,0 +1,17 @@
+
+object Filter {
+
+  def foldRight[A, B](as: List[A], z: B)(f: (A, B) => B): B =
+    as match {
+      case Nil => z
+      case x :: xs => f(x, foldRight(xs, z)(f))
+    }
+
+  def main(args: Array[String]) {
+    println(flatmap(List(1, 2, 3, 4))((x: Int) => List("a","b")))
+  }
+
+  def flatmap[A, B](as: List[A])(f: A => List[B]): List[B] = {
+    foldRight(as, Nil: List[B])((x, y) => foldRight(f(x), y)((m, n) => m :: n))
+  }
+}
